e4e81b824908adf75b1c94f88642456bfe852e31
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-02-12 11:40:36 -0700

    Added all files

---
+++ Assets/Scripts/DeimosMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeimosMove : MonoBehaviour {
+
+	public Rigidbody2D player;
+	public float moveSpeed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		player.velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, player.velocity.y);
+	}
+}
