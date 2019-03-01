4a9418c89cb49b57e028a80cb756fb36f66b1f4b
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-02-28 18:38:08 -0700

    Added light attack Animation

---
+++ Assets/Scripts/DeimosAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeimosAttack : MonoBehaviour
+{	
+	public Rigidbody2D player;
+	public Animator animator;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        	animator.SetBool("Attack",true);
+        else
+        	animator.SetBool("Attack",false);
+        
+    }
+}
