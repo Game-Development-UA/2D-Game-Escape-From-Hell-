b9daf0f3c84343e1222c46af8368c37385839944
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-03-02 17:25:30 -0700

    Created Prefab for attack from deimos

--- Assets/Scripts/SmallMonster.cs
+++ Assets/Scripts/SmallMonster.cs
@@ -7,9 +7,9 @@ public class SmallMonster : MonoBehaviour
 
 	public float hp;
 	public float dmg;
-	/*void OnTriggerEnter2D( Collider2D col ) {
+	void OnTriggerEnter2D( Collider2D col ) {
 		Deimos deimos = col.gameObject.GetComponent<Deimos>();
 		hp--;
-		//deimos.transform = Vector3();
-	}*/
+		deimos.AddForce( new Vector2( -2f, 0f ), ForceMode2D.Impulse );
+	}
 }
