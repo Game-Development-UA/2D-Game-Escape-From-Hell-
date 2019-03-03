409f164da78448793ffa20e72b3a933570580dfc
Drew Castalia <dcastalia@email.arizona.edu>
2019-03-03 13:23:46 -0700

    Added interaction between Deimos and monsters
    Also removed accidental Git submodule in version control

--- Assets/Scripts/DeimosController.cs
+++ Assets/Scripts/DeimosController.cs
@@ -10,8 +10,17 @@ public class DeimosController : MonoBehaviour {
 	public float moveThreshHold;
 	public float jumpPower;
 	public float health;
+	public float damage;
+	public float stunnedVelocityThreshold;
+
+	bool stunned;
 
 	void Update(){
+
+		if( stunned ) {
+			if( player.velocity.sqrMagnitude < stunnedVelocityThreshold ) stunned = false;
+		}
+
 		animator.SetBool("Moving",Mathf.Abs(player.velocity.x)  > moveThreshHold);
 		if( Input.GetButtonDown("Jump") ) {
 			player.AddForce( new Vector2( 0f, jumpPower ), ForceMode2D.Impulse );
@@ -23,14 +32,29 @@ public class DeimosController : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void FixedUpdate () {
+		if( stunned ) return;
+
 		player.velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, player.velocity.y);
 	}
 
-	void OnTriggerEnter2D( Collider2D col ) {
+	void OnCollisionEnter2D( Collision2D col ) {
 		SmallMonster monster = col.gameObject.GetComponent<SmallMonster>();
 		if( monster != null ) {
-			health -= monster.dmg;
-			Destroy( monster.gameObject );
+			monster.TakeDamage( this.damage );
+		}
+	}
+
+	public void TakeDamageFrom( Transform enemy, float damageToTake, float forceToPush ) {
+		health -= damageToTake;
+
+		Vector3 intervalFromEnemy = this.transform.position - enemy.position;
+		intervalFromEnemy.y = 0f; // flatten the force so that Deimos is only pushed left or right, never up or down
+		intervalFromEnemy = Vector3.Normalize( intervalFromEnemy );
+
+		player.AddForce( intervalFromEnemy * forceToPush, ForceMode2D.Impulse );
+
+		if( forceToPush > 0f ) {
+			stunned = true;
 		}
 	}
 }
--- Assets/Scripts/SmallMonster.cs
+++ Assets/Scripts/SmallMonster.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class SmallMonster : MonoBehaviour
 {
-
 	public float hp;
 	public float dmg;
-	/*void OnTriggerEnter2D( Collider2D col ) {
-		Deimos deimos = col.gameObject.GetComponent<Deimos>();
-		hp--;
-		deimos.AddForce(new Vector2( -2f, 0f ), ForceMode2D.Impulse );
-	}*/
+	public float push;
+
+	void OnCollisionEnter2D( Collision2D col ) {
+		print( "Monster hit " + col.gameObject );
+		DeimosController deimos = col.gameObject.GetComponent<DeimosController>();
+
+		if( deimos ) {
+			deimos.TakeDamageFrom( this.transform, dmg, push );
+		}
+	}
+
+	public void TakeDamage( float damageToTake ) {
+		hp -= damageToTake;
+
+		if( hp <= 0f ) {
+			Destroy( this.gameObject );
+		}
+	}
 }
