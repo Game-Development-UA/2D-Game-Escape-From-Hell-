3077b04bc9f4468872976b7459590106aa110c75
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-03-06 00:38:52 -0800

    Added Thanatos script(Unfinished)

--- Assets/Scripts/LargeMonster.cs
+++ Assets/Scripts/LargeMonster.cs
@@ -19,20 +19,12 @@ public class LargeMonster : MonoBehaviour
 			deimos.TakeDamageFrom( this.transform, dmg, push );
 		}
 	}
-
-	/*public void TakeDamage( float damageToTake ) {
-		hp -= damageToTake;
-
-		if( hp <= 0f ) {
-			Destroy( this.gameObject );
-		}
-	}*/
 	public void Update(){
 		if(Mathf.Abs(this.transform.position.x - deimos.transform.position.x)<= distanceToAttack){
 			animator.SetBool("AttackMonster", true);
 			Vector3 newPos = this.transform.position;
 			newPos.x -=Time.deltaTime*speed;
-			this.transform.position = newPos;		//transform.position.x += Time.deltaTime*speed;
+			this.transform.position = newPos;
 		}
 		else{
 			animator.SetBool("AttackMonster", false);
--- Assets/Scripts/SmallMonster.cs
+++ Assets/Scripts/SmallMonster.cs
@@ -6,7 +6,7 @@ public class SmallMonster : MonoBehaviour
 {
 	public float dmg;
 	public float push;
-	HealthMonsters health;
+	public HealthMonsters health;
 
 	void OnCollisionEnter2D( Collision2D col ) {
 		print( "Monster hit " + col.gameObject );
@@ -16,12 +16,4 @@ public class SmallMonster : MonoBehaviour
 			deimos.TakeDamageFrom( this.transform, dmg, push );
 		}
 	}
-
-	/*public void TakeDamage( float damageToTake ) {
-		hp -= damageToTake;
-
-		if( hp <= 0f ) {
-			Destroy( this.gameObject );
-		}
-	}*/
 }
