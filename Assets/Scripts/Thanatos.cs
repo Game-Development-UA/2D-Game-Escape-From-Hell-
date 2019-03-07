e46a4749c616a48ba074cb6d5a8da1de32e540c3
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-03-06 23:09:55 -0800

    Added script for thanatosAttack (Unfinished)

--- Assets/Scripts/Thanatos.cs
+++ Assets/Scripts/Thanatos.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Thanatos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float dmg;
+	public float push;
+	public HealthMonsters health;
+	public float distanceToAttack;
+	public float intervalToChangePos;
+	public float intervalToAttack;
+	public List<ThanatosAttack> attacks = new List<ThanatosAttack>();
+	public Transform attackSpawnLoc;
+	public int maxAttacks;
+	public DeimosController deimos;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnCollisionEnter2D( Collision2D col ) {
+		DeimosController deimos = col.gameObject.GetComponent<DeimosController>();
+		if( deimos ) {
+			deimos.TakeDamageFrom( this.transform, dmg, push );
+		}
+	}
+	public void Update(){
+		if(this.transform.position.x - deimos.transform.position.x <= distanceToAttack){
+			if( attacks.Count < maxAttacks ) {
+				ThanatosAttack newAttack = Instantiate<ThanatosAttack>(ThanatosAttack);
+				newAttack.transform.position = attackSpawnLoc.position;
+				newAttack.deimos = this;
+				attacks.Add(newAttack);
+			}
+		}
+	}
+
 }
