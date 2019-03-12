d9f982beeec8083892f36ef475cf907ceafedd09
Atush Bhalla <atushbhalla@email.arizona.edu>
2019-03-11 18:39:26 -0700

    Longer Game

--- Assets/Scripts/SmallMonster.cs
+++ Assets/Scripts/SmallMonster.cs
@@ -7,12 +7,16 @@ public class SmallMonster : MonoBehaviour
 	public float dmg;
 	public float push;
 	public HealthMonsters health;
+	public AudioSource dyingSound;
 
 	void OnCollisionEnter2D( Collision2D col ) {
 		DeimosController deimos = col.gameObject.GetComponent<DeimosController>();
 
 		if( deimos ) {
 			deimos.TakeDamageFrom( this.transform, dmg, push );
+		}
+		if(health.hp <=0 ){
+			dyingSound.Play();
 		}
 	}
 }
